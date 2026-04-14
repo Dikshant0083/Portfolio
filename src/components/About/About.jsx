82e13539cb86dae975d1925dbe4bc593e2bd0d3e
dikshant0083 <khileridikshant@gmail.com>
2026-04-14 23:43:25 +0530

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1U3JmZ--ojYR4IqWH5eoD5VhC0Ev1MVa3/view?usp=sharing"
+            href="https://drive.google.com/file/d/1xUyvYQvH7i9jp8wQxe3jIqwIHhYiuWsU/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
